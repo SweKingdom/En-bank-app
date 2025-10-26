5c53e38de1000fa1dfdb6d6479f99b6c9a78d044
Philip Oxelius <philip.oxelius@gmail.com>
2025-10-26 13:17:36 +0100

    Basic History with filter/Sorting, Trans and account creating works now

--- BlazorApp4/Domain/Transaction.cs
+++ BlazorApp4/Domain/Transaction.cs
@@ -1,29 +1,21 @@
-﻿namespace BlazorApp4.Domain
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BlazorApp4.Domain
 {
+
+
     public class Transaction
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public TransactionType transactionType { get; set; }
+        public decimal Amount { get; set; }
+        public Currency Currency { get; set; }
+        public decimal BalanceAfterTransaction { get; set; }
         public Guid? FromAccountId { get; set; }
         public Guid? ToAccountId { get; set; }
-        public decimal Amount { get; set; }
-        public decimal BalanceAfter { get; set; }
-        public DateTime TimeStamp { get; set; }
-        public TransactionType Type { get; set; } // <-- använd kort "Type"
 
-        // Parameterlös konstruktor (stöd för object initializers)
-        public Transaction()
-        {
-            TimeStamp = DateTime.Now;
-        }
 
-        // Praktisk konstruktor du kan använda direkt
-        public Transaction(Guid? fromAccountId, Guid? toAccountId, decimal amount, TransactionType type)
-        {
-            FromAccountId = fromAccountId;
-            ToAccountId = toAccountId;
-            Amount = amount;
-            Type = type;
-            TimeStamp = DateTime.Now;
-        }
+
     }
 }
--- BlazorApp4/Interfaces/IBankAccount.cs
+++ BlazorApp4/Interfaces/IBankAccount.cs
@@ -11,14 +11,15 @@ public interface IBankAccount
 {
     Guid Id { get; }
     string Name { get; }
-    Domain.AccountType AccountType { get; }
+    AccountType AccountType { get; }
     Currency Currency { get; }
     decimal Balance { get; }
     DateTime LastUpdated { get; }
 
+    List<Transaction> Transactions { get; }
+
     void Withdraw(decimal amount);
     void Deposit(decimal amount);
 
-    void TransferTo(BankAccount ToAccount, decimal amount);
-    
+    void TransferTo(BankAccount toAccount, decimal amount);
 }
